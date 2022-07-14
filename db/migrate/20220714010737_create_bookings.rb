397a349959c7683c527a3805453c900b9182696e
Donald Heal <donnie.heal@gmail.com>
2022-07-14 11:52:56 +1000

    created both users and bookings database's with basic seeded infomation along with the users and booking controllers

---
+++ db/migrate/20220714010737_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.integer :user_id, foreign_key: true
+      t.integer :time
+      t.string :date
+      t.string :instrument
+
+      t.timestamps
+    end
+  end
+end
