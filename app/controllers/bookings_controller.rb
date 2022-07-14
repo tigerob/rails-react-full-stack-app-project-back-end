436d213cb0c30e69e44350791a0e9e3a12a31235
Donald Heal <donnie.heal@gmail.com>
2022-07-14 12:35:05 +1000

    created pages, users, bookings views and pages controller.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,3 @@
 class BookingsController < ApplicationController
+    # Needs to be logged in to be able to access create a new booking if unautherized to do so display a message to notify to login or sign up
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,3 @@
 class UsersController < ApplicationController
+    # The users controller where creating a new user is performed and all thing pretaning to users is done.
 end
