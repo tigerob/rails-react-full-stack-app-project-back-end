d2d0557395bd8d614afd7b402691c9d96b0aa423
Donald Heal <donnie.heal@gmail.com>
2022-07-22 09:29:09 +1000

    added a few prices controller functions

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,2 +1,31 @@
-class PagesController < ApplicationController
+class PricesController < ApplicationController
+before_action :is_admin, only: [:update]
+before_action :auth_params, only: [:update]
+
+    def index
+        @prices = Price.all
+        render json: @prices
+    end
+
+    def update
+                @price.update(auth_params)
+            if @price.save
+                redirect_to "/price", notice: "Your profile has been updated successfully."
+            else
+                redirect_to "/", alert: "Something went wrong with your changes please try again."
+            end
+    end
+
+    private
+
+    def is_admin
+        unless current_user.is_admin == true
+            redirect_to '/', :alert => "Don't have permission!"
+        end
+    end
+
+    def auth_params
+        (params.require(:prices).permit(:price))
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-
+    resources :prices
+    resources :users
+    resources :bookings
 end
