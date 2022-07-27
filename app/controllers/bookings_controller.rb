524814fd402943fadacf450578f2c8e9d875cfea
tigerob <obrienxyz@gmail.com>
2022-07-27 23:09:51 +1000

    Add 'authenticate user' in bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,3 +1,48 @@
 class BookingsController < ApplicationController
-    # Needs to be logged in to be able to access create a new booking if unautherized to do so display a message to notify to login or sign up
+    before_action :authenticate_user
+    before_action :set_booking, only: [:show, :update, :destroy]
+    
+    def index
+        @bookings = Booking.all
+
+        render json: @bookings
+    end
+
+    def show
+        if @booking
+            render json: @booking
+        else
+            render json: { error: 'Booking not found' }, status: 404
+        end
+    end
+
+    def create
+        @booking = current_user.bookings.create(booking_params)
+        if @booking.save
+            render json: @booking, status: :created
+        else
+            render json: @booking.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @booking.update(booking_params)
+            render json: @booking
+        else
+            render json: @booking.errors, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy
+        @booking.destroy
+    end
+
+    private
+    def set_booking
+        @booking = Booking.find(params[:id])
+    end
+
+    def booking_params
+        params.permit(:text)
+    end
 end
