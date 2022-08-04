105c2510d4537de273cd8f3c9b12721de59e8a10
Donald Heal <donnie.heal@gmail.com>
2022-08-04 21:05:55 +1000

    editing cors again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
     origins "localhost:3000", "localhost:3001", "localhost:3002", "localhost:3006", "mia-music-app.netlify.com", 
-
     resource "*",
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head]
