243bb387c2c357e0d1bae78259a57c09b9173f1f
Donald Heal <donnie.heal@gmail.com>
2022-08-06 06:13:31 +1000

    added ways for admin to edit user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy]
 
     def index
         @users = User.all
 
         render json: @users
+    end
+
+    def update
+        if @user.update(user_params)
+            render json: @user
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
     end
 
     def create
@@ -37,9 +46,17 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+    end
+
     private
     def user_params
-        params.permit(:username, :email, :password, :password_confirmation )
+        params.permit(:username, :email, :password, :password_confirmation, :is_admin, :id )
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
